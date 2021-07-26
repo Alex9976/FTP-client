a4ffa9a40f33a69cce51b1bc6f4cffd9a8e2163f
Alexander <60441130+Alex9976@users.noreply.github.com>
2021-07-27 00:25:09 +0300

    Add item model, add page design

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +25,63 @@ namespace FTP_client
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public MainPage()
+        ObservableCollection<Item> contact = new ObservableCollection<Item>
         {
+            new Item {Name="System Volume Information", Size="121", Type=ItemType.File, LastModificate=DateTime.Now },
+            new Item {Name="a", Size="Folder", Type=ItemType.Folder, LastModificate=DateTime.Now  },
+            new Item {Name="a", Size="121", Type=ItemType.File, LastModificate=DateTime.Now  },
+            new Item {Name="a", Size="121", Type=ItemType.File, LastModificate=DateTime.Now  },
+            new Item {Name="a", Size="121", Type=ItemType.File, LastModificate=DateTime.Now  }
+        };
+
+        public MainPage()
+{
             this.InitializeComponent();
+
+
+            DirectoryList.ItemsSource = contact;
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            //ProgressRing.IsActive = true;
+            ToggleButton.Content = "Disconnect";
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            //ProgressRing.IsActive = false;
+            ToggleButton.Content = "Connect";
+        }
+
+        private void CerateFolder_Click(object sender, RoutedEventArgs e)
+        {
+            contact.Add(new Item { Name = "a", Size = "121", Type = ItemType.File, LastModificate = DateTime.Now });
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem item = (MenuFlyoutItem)sender;
+            DisplayDeleteFileDialog(item.DataContext as Item);
+        }
+
+        private async void DisplayDeleteFileDialog(Item item)
+        {
+            ContentDialog deleteFileDialog = new ContentDialog
+            {
+                Title = $"Delete {item.Type.ToString().ToLower()} permanently?",
+                Content = $"If you delete this {item.Type.ToString().ToLower()}, you won't be able to recover it. Do you want to delete it?",
+                PrimaryButtonText = "Delete",
+                CloseButtonText = "Cancel",
+                DefaultButton = ContentDialogButton.Close
+            };
+
+            ContentDialogResult result = await deleteFileDialog.ShowAsync();
+
+            if (result == ContentDialogResult.Primary)
+            {
+                contact.Remove(item);
+            }
         }
     }
 }
